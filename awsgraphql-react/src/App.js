473112d71cdd639c3eec0b9757dca034d8c71753
Kevin Kabore <kevin.s.kabore@gmail.com>
2020-03-01 14:28:37 -0800

    init tutorial repo

---
+++ awsgraphql-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CreatePost from './CreatePost';
+import DisplayPosts from './DisplayPosts';
+
+function App() {
+	return (
+		<div className="App">
+			<CreatePost />
+			<DisplayPosts />
+		</div>
+	);
+}
+
+export default App;
